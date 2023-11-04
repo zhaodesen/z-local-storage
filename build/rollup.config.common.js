a6824e73d619a55aa4b9baf6cb5526b2e4f22552
Zhao Desen <ZhaoEvan1995@gmail.com>
2023-11-04 20:32:42 +0800

    chore: build config

--- build/rollup.config.common.js
+++ build/rollup.config.common.js
@@ -3,9 +3,8 @@ import base from './rollup.config.base.js'
 const config = Object.assign({}, base[0], {
 	output: {
 		name: 'ZLocalStorage',
-    file: 'dist/z-local-storage.esm.js',
-    format: 'es',
-		sourcemap: true,
+    file: 'dist/z-local-storage.common.js',
+    format: 'cjs',
   },
 })
 
